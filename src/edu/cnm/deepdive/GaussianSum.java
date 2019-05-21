82fc81b038e02a7fbb18b21db3b645088b6ba866
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-05-21 10:32:45 -0600

    Added ability to notify user of invalid input entry.

--- src/edu/cnm/deepdive/GaussianSum.java
+++ src/edu/cnm/deepdive/GaussianSum.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 
 public class GaussianSum {
 
-    public static void main(String[] args) {
-        int upperLimit;
-        long sum;
-        Scanner input = new Scanner(System.in);
-        upperLimit = -1;
-        while (upperLimit < 1 || upperLimit > 1000000) {
-            System.out.println("Please enter a number that is between 1 and 1000000.");
-            upperLimit = input.nextInt();
-        }
-        sum = (long) upperLimit * (upperLimit + 1) / 2;
-        System.out.println("The gaussian sum of " + upperLimit + " is " + sum);
+  public static void main(String[] args) {
+    int upperLimit;
+    long sum;
+    Scanner input = new Scanner(System.in);
+    upperLimit = -1;
+    while (upperLimit < 1 || upperLimit > 1000000) {
+      System.out.println("Please enter a number that is between 1 and 1000000.");
+      while (!input.hasNextInt()) {
+        System.out.println ("That value is not a valid entry. Please enter a number between 1 and 100000.");
+        input.next();
+      }
+      upperLimit = input.nextInt();
     }
+    sum = (long) upperLimit * (upperLimit + 1) / 2;
+    System.out.println("The gaussian sum of " + upperLimit + " is " + sum);
+  }
 }
